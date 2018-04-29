2828171e7de5470a0630a68f062d054db3d81d22
Dimmy Magalhães <dimmyk@gmail.com>
2018-04-29 12:16:15 -0300

    Prediction method

--- model/user.py
+++ model/user.py
@@ -3,4 +3,11 @@ class User(object):
         self.cod = cod
         self.item_ratings = item_ratings
         self.item_with_predictions = []
+        self.similar_users = []
+
+    def __repr__(self):
+        return 'User '+str(self.cod)
+
+    def avg(self):
+        return sum(self.item_ratings[i] for i in range(len(self.item_ratings)) if self.item_ratings[i]>0)/sum(i>0 for i in self.item_ratings)
     pass
