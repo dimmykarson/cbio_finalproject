91907187fe4c4f5c6ea4f44dd4a0c4622c9b299e
Dimmy Magalhães <dimmyk@gmail.com>
2018-05-06 22:55:18 -0300

    ant System

--- ant/ant_system.py
+++ ant/ant_system.py
@@ -3,13 +3,13 @@ from cbio_finalproject.util.Functions import *
 from cbio_finalproject.util.predition import *
 from cbio_finalproject.core.users import load
 
-
+best = 10
 n = 0 #qt_componentes
 C = [] #componentes
-e = random.uniform(0,1) #constante de evaporação
-popsize = 0
+e = 0.01 #constante de evaporação
+popsize = 10
 y = 0 #valor inicial do feromôneo
-t = 0 #interações do hill-climbind
+t = 10 #interações do hill-climbind
 
 L = 10
 
@@ -28,31 +28,40 @@ def components():
             C.append([t, z, predicao, feromonio])
 
 def isBest(sol):
-    return sol[0]>1
+    return fitness(sol)>best
 
 def criar_c_(C, S):
     C_ = C[:]
+    to_remove = []
+    l = get_L(S)
     for s in S:
         to_remove = []
         for c in C_:
             if c[0]==s[0]:
                 to_remove.append(c)
-    for tr in to_remove:
-        C_.remove(tr)
+                continue
+            if (c[1]+l)>L:
+                to_remove.append(c)
+                continue
+
+    if not empty(to_remove):
+        for tr in to_remove:
+            C_.remove(tr)
     return C_
+
+def get_L(S):
+    return sum([x[1] for x in S])
 
 
 def selecionar(S, C_):
-    i = len(S)
-    Best = None
-    for c in C_:
-        if not c[0]==i:
-            continue
-        if Best[2]==0 or c[2]==0:
-            continue
-        if Best == None or (c[3]*1/c[2])>(Best[3]*1/Best[2]):
-            Best = c
-    return Best
+    i = len(S)+1
+    x = [z for z in C_ if z[0]==i]
+    avg = sum(h[3] for h in x)/len(x)
+    x_ = [z for z in x if z[3]>avg]
+    if empty(x_):
+        return random.choice(x)
+    else:
+        return random.choice(x_)
 
 def complete_trail(S):
     return len(S)==4
@@ -61,9 +70,10 @@ def hill_climb(S, t):
     q = 0
     while q < t:
         s_cp = S[:]
-        R = tweak(s_cp[0])
+        R = tweak(s_cp)
         if fitness(R)>fitness(S):
             S = R
+        q=q+1
     return S
 
 def select_random(cod):
@@ -72,7 +82,20 @@ def select_random(cod):
 
 
 def tweak(S):
-    r = random.randint(0, 3)
+    r1 = random.randint(1, 4)
+    r2 = random.randint(1, 4)
+    z1 = S[r1-1][1]
+    z2 = S[r2-1][1]
+
+
+    #pegar aleatoriamente um item dos componentes de cod = r e adicionar na solução
+    x1 = [z for z in C if z[0]==r1 and z[1]==z2]
+    x2 = [z for z in C if z[0]==r2 and z[1]==z1]
+
+    item1 = random.choice(x1)
+    item2 = random.choice(x2)
+    S[r1-1]=item1
+    S[r2-1]=item2
 
     return S
 
@@ -80,9 +103,14 @@ def assess_fitness(S):
     return S
 
 def fitness(S):
-    return S[4]
+    fit = sum(r[2] for r in S)
+    qt_itens = sum(r[1] for r in S)
+    if fit == 0:
+        return 0
+    return (1/fit)*qt_itens
 
-def run():
+def run(pop_size):
+    components()
     Best = None
     qt_interacoes = 0
     while True:
@@ -96,10 +124,11 @@ def run():
                     S = []
                 else:
                     S.append(selecionar(S, C_))
+                if len(S)==4 and get_L(S)<10:
+                    S = []
                 if complete_trail(S):
                     break
             S = hill_climb(S, t)
-            assess_fitness(S)
             if Best == None or fitness(S) > fitness(Best):
                 Best = S
             P.append(S)
@@ -119,3 +148,8 @@ def run():
 
         if isBest(Best) or qt_interacoes > interacoes:
             break
+    return Best
+
+for i in range(10):
+    Best = run(popsize)
+    print("Solução %d. Fitness: %f. dados: %s" % (i, fitness(Best), str(Best)))
